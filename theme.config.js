666c541dd94feb3a949a894fc90acdd6b9e5f9dd
tianyoulan <tianyoulan@autox.ai>
2023-03-22 10:21:09 +0800

    feat: change theme and remove prettier

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
-const YEAR = new Date().getFullYear()
+const YEAR = new Date().getFullYear();
 
 export default {
-  footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © 兰天游.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
+	footer: (
+		<small style={{ display: "block", marginTop: "8rem" }}>
+			<time>{YEAR}</time> © 兰天游.
+			<a href="/feed.xml">RSS</a>
+			<style jsx>{`
         a {
           float: right;
         }
@@ -16,6 +16,12 @@ export default {
           }
         }
       `}</style>
-    </small>
-  )
-}
+		</small>
+	),
+	navs: [
+		{
+			url: "/demos",
+			name: "Demos",
+		},
+	],
+};
